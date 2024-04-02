3937d296fd0a6a58685fa4a2065c9753523632e0
Sébastien Chopin <seb@nuxt.com>
2024-04-02 17:56:52 +0200

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxthub/core'],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    experimental: {
+      // Enable Server API documentation within NuxtHUb
+      openAPI: true
+    }
+  }
 })
