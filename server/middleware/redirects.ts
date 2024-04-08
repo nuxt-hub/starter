292e8cfdf52ad6ffe29e87785f6be4fdd673d488
Sébastien Chopin <seb@nuxt.com>
2024-04-08 23:07:28 +0200

    fix: skpi during prerendering

--- server/middleware/redirects.ts
+++ server/middleware/redirects.ts
@@ -1,6 +1,9 @@
 import { parseURL } from 'ufo'
 
 export default eventHandler(async (event) => {
+  // Skip during prerendering
+  if (import.meta.prerender) return
+
   const { pathname } = parseURL(event.path)
   const redirects = await hubKV().get<{ [key: string]: string }>('redirects')
 
