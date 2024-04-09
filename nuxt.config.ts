fc2c2dda3ce437d5e6e51f57a1780ebf7b2d08a1
Sébastien Chopin <seb@nuxt.com>
2024-04-09 10:30:40 +0200

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxthub/core'],
+  hub: {
+    database: true,
+    kv: true,
+    blob: true,
+    cache: true,
+  },
   nitro: {
     experimental: {
       // Enable Server API documentation within NuxtHub
