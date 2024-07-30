c0b4f61e676b93e85de9fbd7230201945f5cd17f
Sébastien Chopin <seb@nuxt.com>
2024-07-30 17:27:11 +0200

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-07-30',
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
