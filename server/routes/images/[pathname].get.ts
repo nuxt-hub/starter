29fd12e65ed20b81c4da1ed563a3e9c02006725c
Sébastien Chopin <seb@nuxt.com>
2024-10-23 01:05:40 +0200

    chore: update deps

--- server/routes/images/[pathname].get.ts
+++ server/routes/images/[pathname].get.ts
@@ -1,5 +1,6 @@
 export default eventHandler(async (event) => {
   const { pathname } = event.context.params || {}
 
+  setHeader(event, 'Content-Security-Policy', 'default-src \'none\';')
   return hubBlob().serve(event, pathname)
 })
